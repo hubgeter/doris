f278297963ea98f1189c02800fc8353b2dbc13df
Qi Chen <chenqi@selectdb.com>
2024-12-25 12:09:34 +0800

    [tmp](utf8) Disable utf8 validation temporaily. (#266)

--- be/src/util/utf8_check.cpp
+++ be/src/util/utf8_check.cpp
@@ -329,9 +329,6 @@ bool validate_utf8(const char* src, size_t len) {
 #endif
 
 bool validate_utf8(RuntimeState* state, const char* src, size_t len) {
-    if (state->query_options().enable_text_validate_utf8) {
-        return validate_utf8(src,len);
-    }
     return true;
 }
 } // namespace doris
